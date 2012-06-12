cdd736407ca1bdcb5e419ed1715b86c06a609594
Kelsey Hightower <kelsey@puppetlabs.com>
2012-06-12 10:16:56 -0400

    (#14867) Add windows support
    Without this patch Hiera does not work on Windows and fails with the
    following error:
    
        Failed to start Hiera: RuntimeError: Config file /etc/hiera.yaml not
        found
    
    Following the standard set by the Puppet installer, Hiera now uses the
    following configuration and var directories on Windows:
    
        C:\ProgramData\PuppetLabs\hiera\etc
        C:\ProgramData\PuppetLabs\hiera\var
    
    This patch introduces a new `Hiera::Util` module which utilizes the
    `win32-dir` gem for locating the correct base directories on Windows.

---
+++ lib/hiera/util.rb
@@ -0,0 +1,39 @@
+class Hiera
+  module Util
+    module_function
+
+    def posix?
+      require 'etc'
+      Etc.getpwuid(0) != nil
+    end
+
+    def microsoft_windows?
+      return false if posix?
+      
+      begin
+        require 'win32/dir'
+        true
+      rescue LoadError => err
+        warn "Cannot run on Microsoft Windows without the win32-dir gem: #{err}"
+        false
+      end
+    end
+
+    def config_dir
+      if microsoft_windows?
+         File.join(Dir::COMMON_APPDATA, 'PuppetLabs', 'hiera', 'etc')
+      else
+        '/etc'
+      end
+    end
+
+    def var_dir
+      if microsoft_windows?
+        File.join(Dir::COMMON_APPDATA, 'PuppetLabs', 'hiera', 'var')
+      else
+        '/var/lib/hiera'
+      end
+    end
+  end
+end
+
