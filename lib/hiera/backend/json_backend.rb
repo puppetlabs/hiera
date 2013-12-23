0c4229e3da65094e39d9947c06dfafe5be875b1f
Adrian Lopez <adrianlzt@gmail.com>
2013-12-23 16:57:20 +0100

    (HI-120) Allow wildcards in Hiera hierarchy It permits to use wildcards (use Dir.glob) to define datasource files in the Hiera hierarchy

--- lib/hiera/backend/json_backend.rb
+++ lib/hiera/backend/json_backend.rb
@@ -17,36 +17,42 @@ class Hiera
         Backend.datasources(scope, order_override) do |source|
           Hiera.debug("Looking for data source #{source}")
 
-          jsonfile = Backend.datafile(:json, scope, source, "json") || next
+          jsonfiles = Backend.datafile(:json, scope, source, "json") || next
 
-          next unless File.exist?(jsonfile)
+          next if jsonfiles.empty?
 
-          data = @cache.read_file(jsonfile, Hash) do |data|
-            JSON.parse(data)
-          end
+          # To pass tests
+          jsonfiles = [jsonfiles] if jsonfiles.class == String
 
-          next if data.empty?
-          next unless data.include?(key)
+          jsonfiles.each do |jsonfile|
 
-          # for array resolution we just append to the array whatever
-          # we find, we then goes onto the next file and keep adding to
-          # the array
-          #
-          # for priority searches we break after the first found data item
-          new_answer = Backend.parse_answer(data[key], scope)
-          case resolution_type
-          when :array
-            raise Exception, "Hiera type mismatch: expected Array and got #{new_answer.class}" unless new_answer.kind_of? Array or new_answer.kind_of? String
-            answer ||= []
-            answer << new_answer
-          when :hash
-            raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
-            answer ||= {}
-            answer = Backend.merge_answer(new_answer,answer)
-          else
-            answer = new_answer
-            break
-          end
+            data = @cache.read_file(jsonfile, Hash) do |data|
+              JSON.parse(data)
+            end
+  
+            next if data.empty?
+            next unless data.include?(key)
+  
+            # for array resolution we just append to the array whatever
+            # we find, we then goes onto the next file and keep adding to
+            # the array
+            #
+            # for priority searches we break after the first found data item
+            new_answer = Backend.parse_answer(data[key], scope)
+            case resolution_type
+            when :array
+              raise Exception, "Hiera type mismatch: expected Array and got #{new_answer.class}" unless new_answer.kind_of? Array or new_answer.kind_of? String
+              answer ||= []
+              answer << new_answer
+            when :hash
+              raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
+              answer ||= {}
+              answer = Backend.merge_answer(new_answer,answer)
+            else
+              answer = new_answer
+              break
+            end
+	  end
         end
 
         return answer
