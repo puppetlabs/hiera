b11d32f632aed4e2d7a365dbf102abeae0389479
Michael Smith <michael.smith@puppetlabs.com>
2014-12-19 14:04:58 -0800

    Revert "Revert "(HI-304) Make order override work with interpolation""

--- lib/hiera/interpolate.rb
+++ lib/hiera/interpolate.rb
@@ -9,12 +9,12 @@ class Hiera::Interpolate
     INTERPOLATION = /%\{([^\}]*)\}/
     METHOD_INTERPOLATION = /%\{(scope|hiera|literal|alias)\(['"]([^"']*)["']\)\}/
 
-    def interpolate(data, scope, extra_data)
+    def interpolate(data, scope, override, extra_data)
       if data.is_a?(String)
         # Wrapping do_interpolation in a gsub block ensures we process
         # each interpolation site in isolation using separate recursion guards.
         data.gsub(INTERPOLATION) do |match|
-          interp_val = do_interpolation(match, Hiera::RecursiveGuard.new, scope, extra_data)
+          interp_val = do_interpolation(match, Hiera::RecursiveGuard.new, scope, override, extra_data)
 
           # Get interp method in case we are aliasing
           if data.is_a?(String) && (match = data.match(INTERPOLATION))
@@ -38,17 +38,17 @@ class Hiera::Interpolate
       end
     end
 
-    def do_interpolation(data, recurse_guard, scope, extra_data)
+    def do_interpolation(data, recurse_guard, scope, override, extra_data)
       if data.is_a?(String) && (match = data.match(INTERPOLATION))
         interpolation_variable = match[1]
         recurse_guard.check(interpolation_variable) do
           interpolate_method, key = get_interpolation_method_and_key(data)
-          interpolated_data = send(interpolate_method, data, key, scope, extra_data)
+          interpolated_data = send(interpolate_method, data, key, scope, override, extra_data)
 
           # Halt recursion if we encounter a literal.
           return interpolated_data if interpolate_method == :literal_interpolate
 
-          do_interpolation(interpolated_data, recurse_guard, scope, extra_data)
+          do_interpolation(interpolated_data, recurse_guard, scope, override, extra_data)
         end
       else
         data
@@ -70,7 +70,7 @@ class Hiera::Interpolate
     end
     private :get_interpolation_method_and_key
 
-    def scope_interpolate(data, key, scope, extra_data)
+    def scope_interpolate(data, key, scope, override, extra_data)
       value = scope[key]
       if value.nil? || value == :undefined
         value = extra_data[key]
@@ -80,18 +80,18 @@ class Hiera::Interpolate
     end
     private :scope_interpolate
 
-    def hiera_interpolate(data, key, scope, extra_data)
-      Hiera::Backend.lookup(key, nil, scope, nil, :priority)
+    def hiera_interpolate(data, key, scope, override, extra_data)
+      Hiera::Backend.lookup(key, nil, scope, override, :priority)
     end
     private :hiera_interpolate
 
-    def literal_interpolate(data, key, scope, extra_data)
+    def literal_interpolate(data, key, scope, override, extra_data)
       key
     end
     private :literal_interpolate
 
-    def alias_interpolate(data, key, scope, extra_data)
-      Hiera::Backend.lookup(key, nil, scope, nil, :priority)
+    def alias_interpolate(data, key, scope, override, extra_data)
+      Hiera::Backend.lookup(key, nil, scope, override, :priority)
     end
     private :alias_interpolate
   end
