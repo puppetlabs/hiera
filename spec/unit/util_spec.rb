f16dbe57c499c3e966a862047e68675eb2f09221
Thomas Hallgren <thomas@tada.se>
2015-02-12 11:18:59 +0100

    (HI-298) Strip 'agent/' part of default paths.
    This commit removes the 'agent' segment from the config_dir and
    var_dir properties of the Util class. It also updates the sample
    and tests (unit and acceptance) to reflect the change.

--- spec/unit/util_spec.rb
+++ spec/unit/util_spec.rb
@@ -23,7 +23,7 @@ describe Hiera::Util do
   describe 'Hiera::Util.config_dir' do
     it 'should return the correct path for posix systems' do
       Hiera::Util.expects(:file_alt_separator).returns(nil)
-      Hiera::Util.config_dir.should == '/etc/puppetlabs/agent/code'
+      Hiera::Util.config_dir.should == '/etc/puppetlabs/code'
     end
 
     it 'should return the correct path for microsoft windows systems' do
@@ -36,7 +36,7 @@ describe Hiera::Util do
   describe 'Hiera::Util.var_dir' do
     it 'should return the correct path for posix systems' do
       Hiera::Util.expects(:file_alt_separator).returns(nil)
-      Hiera::Util.var_dir.should == '/etc/puppetlabs/agent/code/hieradata'
+      Hiera::Util.var_dir.should == '/etc/puppetlabs/code/hieradata'
     end
 
     it 'should return the correct path for microsoft windows systems' do
