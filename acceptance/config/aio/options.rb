ae1e656d1b434e40953c4f0a730fcbdccff58b51
Eric Thompson <eric.thompson@puppetlabs.com>
2015-02-24 15:48:14 -0800

    (maint) HI-345 fix hiera pre-suite for aio
    Before this change the hiera pre-suite could not run puppet to configure
    tests properly.
    This change creates temporary work-arounds until the aio packaging
    creates and links binaries into the path properly.

--- acceptance/config/aio/options.rb
+++ acceptance/config/aio/options.rb
@@ -1,6 +1,8 @@
 {
   :type => 'aio',
+  :puppetbindir => '/opt/puppetlabs/puppet/bin',
   :pre_suite => [
     'setup/aio/pre-suite/010_Install.rb',
+    'setup/aio/pre-suite/020_AIO_Workarounds.rb',
   ],
 }
