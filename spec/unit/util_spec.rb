0efd9276d3ada2c2f65741378a382b8c32a45e0b
Josh Cooper <josh@puppetlabs.com>
2015-02-23 16:53:04 -0800

    (HI-298) Move hiera data to C:\ProgramData\PuppetLabs\code
    Previously, hiera on Windows defaulted to
    C:\ProgramData\PuppetLabs\hiera\etc.
    
    This commit updates hiera to use C:\ProgramData\PuppetLabs\code.

--- spec/unit/util_spec.rb
+++ spec/unit/util_spec.rb
@@ -29,7 +29,7 @@ describe Hiera::Util do
     it 'should return the correct path for microsoft windows systems' do
       Hiera::Util.expects(:microsoft_windows?).returns(true)
       Hiera::Util.expects(:common_appdata).returns('C:\\ProgramData')
-      Hiera::Util.config_dir.should == 'C:\\ProgramData/PuppetLabs/hiera/etc'
+      Hiera::Util.config_dir.should == 'C:\\ProgramData/PuppetLabs/code'
     end
   end
 
@@ -42,7 +42,7 @@ describe Hiera::Util do
     it 'should return the correct path for microsoft windows systems' do
       Hiera::Util.expects(:microsoft_windows?).returns(true)
       Hiera::Util.expects(:common_appdata).returns('C:\\ProgramData')
-      Hiera::Util.var_dir.should == 'C:\\ProgramData/PuppetLabs/hiera/var'
+      Hiera::Util.var_dir.should == 'C:\\ProgramData/PuppetLabs/code/hieradata'
     end
   end
 end
