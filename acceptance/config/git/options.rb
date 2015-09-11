d07982f4e70bfefd247fc5164d068208567a8a39
John Duarte <john.duarte@puppetlabs.com>
2015-09-11 10:15:35 -0700

    (maint) Add acceptance pre-suite to rm puppet on sparc
    This commit adds a beaker pre-suite to ensure that puppet packages
    and components are removed when the platform is on sparc and the
    hypervisor is none.
    
    This ensures that the sparc testing platform does not have any
    remaining puppet bits when it is not being managed by a provisioning
    hypervisor.

--- acceptance/config/git/options.rb
+++ acceptance/config/git/options.rb
@@ -1,6 +1,7 @@
 {
   :install => ['PUPPET/master', 'FACTER/2.x'],
   :pre_suite => [
+    'setup/common/pre-suite/000-delete-puppet-when-sparc.rb',
     'setup/common/00_EnvSetup.rb',
     'setup/git/pre-suite/01_TestSetup.rb',
   ],
