953fae81ad91934d2ce43550c4bc6b9869b35af3
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-10-21 17:07:46 +0300

    Use syck YAML parser for Ruby 1.9.3 and 2.0.x
    Default YAML parser treats unquoted strings starting
    with 0 sometimes as a numeric value.
    
    Example input:
    password: 0Xmy_secret_pass
    mac: 08:00:27:32:56:08
    
    1.93 parser output:
    $ hiera password
    0Xmy_secret_pass
    $ hiera mac
    6226750568
    
    2.0.0 parser output:
    $ hiera password
    12380245
    $ hiera mac
    1960347
    
    Output is as expected with 'syck' yamler in 1.9.3 and 2.0.0:
    $ hiera password
    0Xmy_secret_pass
    $ hiera mac
    08:00:27:32:56:08

--- lib/hiera/config.rb
+++ lib/hiera/config.rb
@@ -65,7 +65,7 @@ class Hiera::Config
     #
     # @return [Object] return value of {YAML.load_file}
     def yaml_load_file(source)
-      if RUBY_VERSION.between("1.9.3","2.0.9")
+      if RUBY_VERSION  >= "1.9.3" and RUBY_VERSION < "2.1.0"
         YAML::ENGINE.yamler = 'syck'
       end
       YAML.load_file(source)
