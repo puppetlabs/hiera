2fe5b59a8d720a5203f929edd79c82d5676673c2
Josh Cooper <josh@puppetlabs.com>
2016-01-06 11:46:23 -0800

    Revert "(HI-489) Add Cumulus Support"
    This reverts commit d9aaa573db4a2ed70fc526299b3417d3a827885b.

--- acceptance/config/aio/options.rb
+++ acceptance/config/aio/options.rb
@@ -3,6 +3,5 @@
   :pre_suite => [
     'setup/common/pre-suite/000-delete-puppet-when-none.rb',
     'setup/aio/pre-suite/010_Install.rb',
-    'setup/aio/pre-suite/020_InstallCumulusModules.rb',
   ],
 }
