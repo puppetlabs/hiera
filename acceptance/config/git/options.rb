d7f7e6fbfb1ebeca3c5e85a4f245fc935728fe11
Melissa Stone <melissa@puppet.com>
2018-03-30 12:24:13 -0700

    (HI-596) Move hiera to running test from beaker-puppet
    This includes removing all the set up logic from hiera. This is now
    stored in beaker-puppet. There is one test that was previously being
    executed during the test phase, but is more appropriate to run as a
    pre-suite. This ensures it is run before any of the other tests, so that
    order isn't as important.

--- acceptance/config/git/options.rb
+++ acceptance/config/git/options.rb
@@ -1,8 +1,6 @@
 {
   :install => ['PUPPET/master', 'FACTER/2.x'],
   :pre_suite => [
-    'setup/common/pre-suite/000-delete-puppet-when-none.rb',
-    'setup/common/00_EnvSetup.rb',
-    'setup/git/pre-suite/01_TestSetup.rb',
+    'setup/common/00-setup.rb'
   ],
 }
